823fcc1792d7a78d502432f95b52583538aa248d
laxmi59 <laxmi.kotni@gmail.com>
2014-12-02 18:09:12 +0530

    2nd Dec commit

--- productManager/data/dataFunctions.php
+++ productManager/data/dataFunctions.php
@@ -1,6 +1,6 @@
 <?php
 include ("config.php");
-if($_GET['rec'] == 'all'){
+if($_GET['act'] == 'all'){
     $sql = "SELECT `id` , `sku` , `name` , `price` , `mrp` , `description` , `packing` , `image` , `category` , `stock` , `status` FROM `products` ORDER BY id ASC";
     $query = mysql_query($sql);                
     $rows = array();
@@ -8,5 +8,36 @@ if($_GET['rec'] == 'all'){
         $rows[] = $r;
     }
     echo json_encode($rows);
+}
+
+if($_GET['act'] == 'statusUpdate'){
+    $sql = "SELECT `id`, `status` FROM `products` where id = ".$_GET['pid'];
+    $query = mysql_fetch_array(mysql_query($sql));                
+    if($query['status'] == 'Active'){
+        $upd = "status = 'Inactive'";
+    }else{
+        $upd = "status = 'Active'";
+    }
+    $updSql = "update `products` set ".$upd." where id = ".$_GET['pid'];
+    $updquery = mysql_query($updSql);
+    if($updquery){
+        echo 1;
+    }else{
+        echo 0;
+    }
+}
+
+if($_GET['act'] == 'insertProduct'){
+    $errorsArr = array();
+    if($_REQUEST['productName'] == "undefined")
+        $errorsArr[] = "Name Required";    
+    
+    if($_REQUEST['productPrice'] == "undefined")
+        $errorsArr[] = "Price Required"; 
+    
+    if($errorsArr)
+        print_r($errorsArr);
+    else
+        echo "success";
 }
 ?>
--- productManager/js/app.js
+++ productManager/js/app.js
@@ -1,12 +1,87 @@
 
 (function(){
-    var app = angular.module('app',[]);
-    app.controller('productsController',["$http", function($http, $scope){        
-        $scope = this;
-        $http.get("data/dataFunctions.php?rec=all").success(function(data){
-            $scope.products = data;
-        });
-    }]);  
+     var app = angular.module('app',[]);
+     app.controller('productsController',["$http","$scope", function($http, $scope){        
+        $scope.products = [];
+        $scope.init = function(){            
+            $http.get("data/dataFunctions.php?act=all").success(function(data){
+                $scope.products = data;
+            });
+        };
+        $scope.init();
+        $scope.changeStatus = function(pid){
+           $http.get("data/dataFunctions.php?act=statusUpdate&pid="+pid).success(function(data){
+               $scope.init();
+            });
+        };        
+        $scope.modalShown = false;
+        $scope.toggleModal = function() {      
+           $scope.modalShown = !$scope.modalShown;
+        };
+        // Product Insertion
+        $scope.errors = [{errors: "test"}, {errors: "test1"}];
+        $scope.insertProduct = function(){ 
+            $http({
+                method: 'POST',
+                url: "data/dataFunctions.php?act=insertProduct",
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                transformRequest: function(obj) {
+                    var str = [];
+                    for(var p in obj)
+                    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                    return str.join("&");
+                },
+                data: {productName: this.productName, productPrice: this.productPrice}
+            }).success(function (data) {
+                alert(data);
+                $scope.errors = data;
+            });
+        };
+    }]);
+    app.directive('modalDialog', function() { 
+        return {
+        restrict: 'E',
+        scope: {
+          show: '='
+        },
+        replace: true, // Replace with the template below
+        transclude: true, // we want to insert custom content inside the directive
+        link: function(scope, element, attrs) {
+          scope.dialogStyle = {};
+          if (attrs.width)
+            scope.dialogStyle.width = attrs.width;
+          if (attrs.height)
+            scope.dialogStyle.height = attrs.height;
+          scope.hideModal = function() {
+            scope.show = false;
+          };
+        },
+        template: "<div class='ng-modal' ng-show='show'><div class='ng-modal-overlay' ng-click='hideModal()'></div><div class='ng-modal-dialog' ng-style='dialogStyle'><div class='ng-modal-close' ng-click='hideModal()'>X</div><div class='ng-modal-dialog-content' ng-transclude></div></div></div>"    
+        };
+    });
+  // Add product directive
+    app.directive('addProducts', function(){
+        return{
+            restrict: 'E',
+            templateUrl: "partials/addProduct.html"
+        }
+    });
+  /*  var app = angular.module('app',[]);
+    app.controller('productsController',["$http","$scope", function($http, $scope){        
+        $scope.products = [];
+        $scope.init = function(){            
+            $http.get("data/dataFunctions.php?act=all").success(function(data){
+                $scope.products = data;
+            });
+        };
+        $scope.init();
+        $scope.changeStatus = function(pid){
+           $http.get("data/dataFunctions.php?act=statusUpdate&pid="+pid).success(function(data){
+               $scope.init();
+            });
+        };      
+        
+    }]);  */
     // product list directive
     app.directive('productsList', function(){
         return{
